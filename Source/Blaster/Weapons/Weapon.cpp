5ae6078477583ba84213e3ee7718e61fae2b94c2
Pierce Provias <p.provias@outlook.com>
2025-05-19 12:11:55 -0400

    Overlap widget is set locally for improved client response when experiencing high ping

--- Source/Blaster/Weapons/Weapon.cpp
+++ Source/Blaster/Weapons/Weapon.cpp
@@ -64,13 +64,12 @@ void AWeapon::BeginPlay()
 	Super::BeginPlay();
 
 	// Collision is only enabled for the server (Weapon must be a replicated actor)
-	if (HasAuthority())
-	{
-		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
-		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
-		AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
-	}
+	
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
+	
 	if (PickupWidget)
 	{
 		UUserWidget* WidgetInstance = PickupWidget->GetUserWidgetObject();
